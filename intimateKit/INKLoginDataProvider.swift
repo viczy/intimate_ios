fe692512b92a4388c9d260fe11897476ab31d174
Zhouhaijian <viczy@ymail.com>
2014-09-26 11:07:11 +0800

    add the afnetworking

---
+++ intimateKit/INKLoginDataProvider.swift
@@ -0,0 +1,21 @@
+//
+//  INKLoginDataProvider.swift
+//  intimate
+//
+//  Created by Vic Zhou on 9/26/14.
+//  Copyright (c) 2014 everycode. All rights reserved.
+//
+
+import Foundation
+
+class INKLoginDataProvider : NSObject {
+    func login (accout:NSString, password:NSString) {
+        return INKHTTPSessionManager.sharedClient().POST(URLString: "", parameters: nil, constructingBodyWithBlock: { (data:AFMultipartFormData!) -> Void in
+            //
+            }, success: { (task:NSURLSessionDataTask, json:NSObject) -> Void in
+            //
+            }, failure: { (task:NSURLSessionDataTask, error:NSError) -> Void in
+            //
+        })
+    }
+}
